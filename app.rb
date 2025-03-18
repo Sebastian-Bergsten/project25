818a2dfdca96703919e6f60984664998a4786bce
Sebastian-Bergsten <sebastian.bergsten@elev.ga.ntig.se>
2025-03-18 16:20:53 +0100

    Update och games
    Fixat klart all CRUD och copierat till games okcså

--- app.rb
+++ app.rb
@@ -12,6 +12,33 @@ get('/')  do
     slim(:start)
 end
 
+get('/games') do
+    db = SQLite3::Database.new("db/DB.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM games")
+    slim(:"games/index",locals:{games:result})
+end
+
+get('/games/new') do
+    slim(:"games/new")
+end
+
+post('/games/new') do
+    title = params[:title]
+    db = SQLite3::Database.new("db/DB.db")
+    db.execute("INSERT INTO games (title) VALUES (?)",title)
+    redirect('/games')
+end
+
+get('/games/:id') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/DB.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM games WHERE GameId = ?",id).first
+    result2 = db.execute("SELECT title FROM achievements WHERE game_id=?",id).first
+    slim(:"games/show",locals:{result:result,result2:result2})
+end
+
 get('/achievements') do
     db = SQLite3::Database.new("db/DB.db")
     db.results_as_hash = true
@@ -38,10 +65,29 @@ end
 post('/achievements/:id/delete') do
     id = params[:id].to_i
     db = SQLite3::Database.new("db/DB.db")
-    db.execute("DELETE FROM achievements WHERE AchievementId = ?",id)
+    db.execute("DELETE FROM achievements WHERE AchievementId=?",id)
     redirect('/achievements')
 end
 
+post('/achievements/:id/update') do
+    id = params[:id].to_i
+    title = params[:title]
+    details = params[:details]
+    link = params[:link]
+    hidden_attribute = params[:hidden_attribute].to_i
+    game_id = params[:game_id].to_i
+    db = SQLite3::Database.new("db/DB.db")
+    db.execute("UPDATE achievements SET title=?, details=?, link=?, hidden_attribute=?, game_id=? WHERE AchievementId=? ",[title,details,link,hidden_attribute,game_id,id])
+    redirect('/achievements')
+end
+
+get('/achievements/:id/edit') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/DB.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM achievements WHERE AchievementId = ?",id).first
+    slim(:"/achievements/edit", locals:{result:result})
+end
 
 get('/achievements/:id') do
     id = params[:id].to_i
